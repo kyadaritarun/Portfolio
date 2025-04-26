da079d9c1eaa70c623f39f8961db37bd877062cd
kyadari tarun <you@example.com>
2025-04-26 19:28:58 +0530

    first commit

---
+++ src/aboutStats.js
@@ -0,0 +1,7 @@
+const aboutStats = {
+  experience: 1, // Years of experience
+  certifications: 5, // Number of certifications
+  education: "Bachelor's in Computer Science and Engineering (2024)", // Education details
+};
+
+export default aboutStats;
---
+++ src/projects.js
@@ -0,0 +1,101 @@
+const projects = [
+    {
+      id: "hyperchat",
+      Img: "/assets/images/hyperchat.jpg",
+      Title: "HyperChat",
+      Description: "Built HyperChat, a real-time internal team communication application using React.js and Node.js with one-on-one and group chat features, real-time user status, push notifications, and message read receipts. Implemented secure authentication (JWT) and end-to-end encryption for data privacy.",
+      Link: "https://hyperchat-t.onrender.com/",
+      TechStack: ["React", "Node.js" , "mongodb", "Tailwind CSS", "JavaScript"],
+      Features: ["Real-time messaging", "User authentication", "Group chat support", " push notifications"],
+      Github: "Private",
+    },
+    {
+      id: "hospital-management",
+      Img: "/images/hospital-management.jpg",
+      Title: "Hospital Management System",
+      Description: "Designed and developed a comprehensive system to streamline patient registration, appointment management, billing, and electronic health records, ensuring efficient hospital operations. ",
+      Link: "#",
+      TechStack: ["Java", "Servlets", "JDBC", "MySQL", "Bootstrap" ,"Front-End Technologies",],
+      Features: ["Patient record management", "Appointment scheduling", "Admin dashboard"],
+      Github: "Private",
+    },
+    {
+      id: "Notes-Reminder",
+      Img: "#",
+      Title: "Notes-Reminder",
+      Description: "An online store with product listings, cart, and payment integration.",
+      Link: "https://reminder-13kp.onrender.com",
+      TechStack: ["React", "Node.js", "Express", "MongoDB"],
+      Features: ["SMS", "CALL"],
+      Github: "Private",
+    },
+    {
+      id: "IT-Company-wed-page",
+      Img: "#",
+      Title: "IT-Company-wed-page",
+      Description: "Ready to take your business to the next level with technology? Let’s connect! Our team is always ready to discuss your ideas, answer your questions, and explore how we can work together to achieve your goals.",
+      Link: "https://kyadaritarun.github.io/IT-Company-project/",
+      TechStack: ["HTML", "CSS", "Java", "MYSQL"],
+      Features: [],
+      Github: "Private",
+    },
+    {
+      id: "Weather-App",
+      Img: "#",
+      Title: "Weather-App",
+      Description: "A Weather App gives users real-time weather updates, forecasts, and alerts based on their location or a searched city/place. It helps people plan their day by showing the temperature, chance of rain, wind speed, and more.",
+      Link: "https://kyadaritarun.github.io/Weather-App/",
+      TechStack: ["HTML", "CSS", "Java", "MYSQL"],
+      Features: ["Live weather" ,"Real-Time Updates"],
+      Github: "Private",
+    },
+    {
+      id: "Travel-Website-Landing-Page",
+      Img: "#",
+      Title: "Travel-Website-Landing-Page",
+      Description: "he Travel Website Landing Page is designed to inspire visitors to explore and book trips. It features a big, beautiful hero section with a catchy heading, a search bar or call-to-action button 'Explore Now', and stunning images of top destinations.",
+      Link: "https://kyadaritarun.github.io/Travel-Website-Landing-Page/",
+      TechStack: ["HTML", "CSS", "Java", "MYSQL"],
+      Features: ["Booking"],
+      Github: "Private",
+    },
+    {
+      id: "Stop-watch",
+      Img: "#",
+      Title: "Stop-watch",
+      Description: "A stopwatch is a tool used to measure the exact amount of time between when an event starts and when it ends.",
+      Link: "https://kyadaritarun.github.io/Stop-watch/",
+      TechStack: ["HTML", "CSS", "Java", "MYSQL"],
+      Features: [],
+      Github: "Private",
+    },
+    {
+      id: "Password-generator",
+      Img: "#",
+      Title: "Password-generator",
+      Description: "A stopwatch is a tool used to measure the exact amount of time between when an event starts and when it ends.",
+      Link: "https://kyadaritarun.github.io/Password-generator/",
+      TechStack: ["HTML", "CSS", "Java", "MYSQL"],
+      Features: [],
+      Github: "Private",
+    },
+    {
+      id: "To-Do-List",
+      Img: "#",
+      Title: "To-Do-List",
+      Description: "A To-Do List is a simple app that helps users organize tasks they need to complete , It lets you add, view, complete, and delete tasks easily.",
+      Link: "https://github.com/kyadaritarun/To-Do-List",
+      TechStack: ["HTML", "CSS", "Java", "MYSQL"],
+      Features: [],
+      Github: "Private",
+    },
+  ];
+
+  export default projects;
+
+
+
+//   Save as .jpg or .png files
+
+
+
